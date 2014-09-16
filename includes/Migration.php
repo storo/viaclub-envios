647e94a2f37921c2751c48d22467b5d40bc11ef6
Sergio Toro <debian.storo@gmail.com>
2014-09-16 00:37:24 -0300

    added migration

--- includes/Migration.php
+++ includes/Migration.php
@@ -8,6 +8,19 @@ class Migration {
     }
 
     function create(){
+        return $this->db->query("CREATE TABLE IF NOT EXISTS vce_events (
+                                 ID INT NOT NULL,
+                                 send_date DATETIME NULL,
+                                 client_id INT NULL,
+                                 user_name VARCHAR(255) NULL,
+                                 user_email VARCHAR(255) NULL,
+                                 recipient_name VARCHAR(255) NULL,
+                                 recipient_email VARCHAR(255) NULL,
+                                 post_id INT NULL,
+                                 PRIMARY KEY (ID))");
+    }
 
+    function delete(){
+        return $this->db->query("DROP TABLE IF EXISTS vce_events");
     }
 }
--- uninstall.php
+++ uninstall.php
@@ -3,3 +3,8 @@
 if (!defined( 'WP_UNINSTALL_PLUGIN' )){
     exit();
 }
+
+require_once 'includes/Migration.php';
+
+$db = new Migration();
+$db->delete();
